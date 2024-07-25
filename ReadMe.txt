1acca6443d9f28d75843e7170b1d25ad19953902
Rayane Bourdim <rayane@Ordinateur-portable-de-Rayane.local>
2024-07-25 10:40:31 +0200

    Initial import

---
+++ ReadMe.txt
@@ -0,0 +1,2 @@
+ng generate component <componant-name> --inline-template --skip-tests
+json-server --watch db.json
---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    HomeComponent,
+    CommonModule,
+    RouterModule
+  ],
+  template: `
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,
+  styleUrls: ['./app.component.css'],
+  
+})
+
+export class AppComponent {
+  title = 'home';
+ 
+
+}
+
+
